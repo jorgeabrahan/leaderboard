42a3799071437ce7e7ff86982698715108d52f63
Jorge <jorge24abrahan@gmail.com>
2023-01-31 17:45:11 -0600

    Removed IDs from scores

--- src/modules/Score.js
+++ src/modules/Score.js
@@ -1,13 +1,11 @@
 export default class Score {
-  constructor(id, name, score) {
-    this.id = id;
+  constructor(name, score) {
     this.name = name;
     this.score = score;
   }
 
   html() {
     const p = document.createElement('p');
-    p.id = this.id;
     p.className = 'score';
     p.innerHTML = `
       <span>${this.name}</span>:<span>${this.score}</span>
--- src/modules/Scores.js
+++ src/modules/Scores.js
@@ -6,21 +6,9 @@ export default class Scores {
     this.container = container;
   }
 
-  add(name, score, id = Scores.generateId()) {
-    const scoreObj = new Score(id, name, score);
+  add(name, score) {
+    const scoreObj = new Score(name, score);
     this.scores.push(scoreObj);
     this.container.appendChild(scoreObj.html());
-  }
-
-  static generateId(tokenLen = 16) {
-    let id = '';
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < tokenLen; i += 1) {
-      let range = chars.length;
-      // First char will always be a letter
-      if (i === 0) range = chars.length - 10;
-      id += chars.charAt(Math.floor(Math.random() * range));
-    }
-    return id;
   }
 }
