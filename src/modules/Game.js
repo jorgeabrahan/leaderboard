91ec55011a2d5210cb2175481fc9ed76d91cd615
Jorge <jorge24abrahan@gmail.com>
2023-01-31 20:06:57 -0600

    Change promise to async/await syntax to consume the API

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -5,54 +5,39 @@ export default class Game {
     this.apiUri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
   }
 
-  create() {
-    return new Promise((resolve, reject) => {
-      if (this.id.length > 0) {
-        resolve(this.id);
-        return;
-      }
+  async create() {
+    if (this.id.length > 0) {
+      return this.id;
+    }
 
-      fetch(`${this.apiUri}games/`, {
-        method: 'POST',
-        body: JSON.stringify({
-          name: this.name,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then(({ result }) => {
-          // result = Game with ID: {id} added.
-          [, , , this.id] = result.split(' ');
-          localStorage.setItem('game_id', this.id);
-          resolve(this.id);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+    const response = await fetch(`${this.apiUri}games/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: this.name,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     });
+    const { result } = response.json();
+    // result = Game with ID: {id} added.
+    [, , , this.id] = result.split(' ');
+    localStorage.setItem('game_id', this.id);
+    return this.id;
   }
 
-  addScore(user, score) {
-    return new Promise((resolve, reject) => {
-      fetch(`${this.apiUri}games/${this.id}/scores`, {
-        method: 'POST',
-        body: JSON.stringify({
-          user,
-          score,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then(({ result }) => {
-          resolve(result);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+  async addScore(user, score) {
+    const response = await fetch(`${this.apiUri}games/${this.id}/scores`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     });
+    const { result } = await response.json();
+    return result;
   }
 }
